6d04a785379d95a6257ed63bef747c08dcd83f79
neurogeek03 <mariaeleni.fafouti@mail.utoronto.ca>
2026-02-18 16:49:08 -0500

    added celltype colored plots

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 How does spatial proximity of specific cell types predict Alzheimer's score?
 
 Data in `h5ad` format available at: https://sea-ad-spatial-transcriptomics.s3.amazonaws.com/index.html#middle-temporal-gyrus/all_donors-h5ad/
+
+## Crucial `.obs` columns: 
+- `Depth from pia`: has a value only for cells that are part of a smaller rechtangular area on the tissue which contains cell types at the required proportions. This type of filtering was used for downstream analyses in the paper. 
+
+## Crucial `.obsm` columns: 
+- `X_spatial_raw_0` , `X_spatial_raw_1`: used to plot 1 section
+- `X_spatial_tiled_0`, `X_spatial_tiled_1`: used to plot all sections together
--- scripts/plot_sections.py
+++ scripts/plot_sections.py
@@ -8,6 +8,7 @@ print(f"current working directory: {project_path}")
 output_base = project_path / 'out'
 output_base.mkdir(exist_ok=True, parents=True)
 sc_ad = sc.read_h5ad("/scratch/mfafouti/SEA-AD/SEAAD_MTG_MERFISH.2024-12-11.h5ad")
+color_csv = pd.read_csv('/scratch/mfafouti/SEA-AD/data/cluster_order_and_colors.csv')
 
 column = 'X_spatial_raw'
 donor_id = 'H20.33.004'
@@ -20,19 +21,31 @@ spatial_df = pd.DataFrame(sc_ad.obsm[column],
 # Add Donor ID to the spatial dataframe
 spatial_df['Section'] = sc_ad.obs['Section'].values
 spatial_df['Donor ID'] = sc_ad.obs['Donor ID'].values
+spatial_df['ct'] = sc_ad.obs['Subclass']
+spatial_df['depth'] = sc_ad.obs['Depth from pia']
+
+# obtaining taxonomy colors 
+subclass_colors = color_csv[['subclass_label', 'subclass_color']].drop_duplicates()
+spatial_df = spatial_df.merge(subclass_colors, left_on='ct', right_on='subclass_label', how='left')
 
 # Subset for a specific donor, e.g., "H20.33.035"
 subset_df = spatial_df[spatial_df['Section'] == section]
-
-coords = pd.DataFrame(sc_ad.obsm[column], columns=["x", "y"])
+# subset_df = subset_df[subset_df['depth'].notna()]
 
 plt.figure(figsize=(12, 9))
-sc = plt.scatter(
-    coords["x"], coords["y"],
-    s=1
+scatter = plt.scatter(
+    subset_df["x"], subset_df["y"],
+    c=subset_df["subclass_color"],
+    s=3
 )
 plt.axis("equal")
 plt.title(f"Spatial plot {section}")
 
-fig_path = output_base / f'{column}_id_{donor_id}.png'
+unique_cts = subset_df[['ct', 'subclass_color']].drop_duplicates().dropna().sort_values('ct')
+handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10, label=label)
+            for label, color in zip(unique_cts['ct'], unique_cts['subclass_color'])]
+plt.legend(handles=handles, title="Subclass", bbox_to_anchor=(1.05, 1), loc='upper left')
+
+plt.tight_layout()
+fig_path = output_base / f'whole_colored_depth_{column}_id_{donor_id}.png'
 plt.savefig(fig_path, dpi=300)
