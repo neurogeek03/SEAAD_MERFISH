dec27639409a201ad64d6b1b321265b392d4f56a
brendanwood29 <brendanwwood22@gmail.com>
2026-03-19 16:04:15 +0000

    [feat] updated trainer to be ABC and have better functionality

--- train.py
+++ train.py
@@ -1,16 +1,13 @@
 import sys
 import re
 import torch
-from utils.callbacks import EarlyStopping
-from utils import get_optim, get_scheduler, get_model, get_loss_fn, MERFISHDataset
+from utils import MERFISHDataset, Trainer
 from omegaconf.dictconfig import DictConfig
 from omegaconf.listconfig import ListConfig
 from omegaconf import OmegaConf
 from tqdm import tqdm
 from pathlib import Path
-import matplotlib.pyplot as plt
 from torch_geometric.loader import DataLoader
-from torchinfo import summary
 
 
 def get_next_run_name(work_dir: str) -> str:
@@ -44,156 +41,24 @@ def get_config() -> ListConfig | DictConfig:
 
     return cfg
 
+class MERFISHTrainer(Trainer):
+    
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        
+    def model_forward(self, batch):
+        
+        y_hat = self.model(batch.x, batch.edge_index, batch.batch)
+        loss = self.loss(y_hat, batch.y)
+        
+        return loss, batch.num_graphs 
 
-class Train():
-    
-    def __init__(self, cfg: ListConfig | DictConfig):
-        self.model = get_model(
-            cfg.model.name, 
-            **cfg.model.kwargs 
-        )
-        self.optimizer = get_optim(
-            cfg.optim.name, 
-            self.model.parameters(), 
-            lr=cfg.optim.lr,
-            **cfg.optim.kwargs
-        )
-        self.loss_fn = get_loss_fn(
-            cfg.loss.name,
-            **cfg.loss.kwargs
-        )
-        
-        if cfg.model.print_summary:
-            summary(self.model)
-        
-        self.scheduler = None
-        if cfg.scheduler is not None:
-            self.lr_history = []
-            self.scheduler = get_scheduler(
-                cfg.scheduler.name, 
-                self.optimizer, 
-                **cfg.scheduler.kwargs
-            )
-        
-        self.stopper = None
-        if cfg.callbacks.use_early_stopping:
-            self.stopper = EarlyStopping(cfg.callbacks.patience)
-        
-        self.cfg = cfg
-        self.loss_epoch = []
-        self.val_loss = []
-        self.step_loss = []
-        self.run_name = cfg.run_name
-        self.work_dir = Path(cfg.work_dir).joinpath(self.run_name)
-        self.work_dir.mkdir(parents=True, exist_ok=True)
-        self.step = 0
-        self.best_val_loss = 100
-        self.top_saved_models = []
-        self.num_models_save = cfg.model.num_models_to_save
-        
-        if cfg.device is not None:
-            self.model.to(cfg.device)
-        else:
-            self.model.to('cuda' if torch.cuda.is_available() else 'cpu')
-    
-    def after_train_batch(self, loss):
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-        
-        if self.scheduler is not None:
-            self.scheduler.step()
-            self.lr_history.append(self.scheduler.get_last_lr())
-
-    
-    def train(self, train_loader: DataLoader):
-        loss_iters = 0
-        self.model.train()
-        for x in train_loader:
-            y_hat = self.model(x.x, x.edge_index, x.batch)
-            loss = self.loss_fn(y_hat, x.y)
-            loss_iters += loss.item()
-            self.step_loss.append(loss.item())
-            self.step += 1
-            self.after_train_batch(loss)
-        self.loss_epoch.append(loss_iters / len(train_loader))
-        
-        
-    def val(self, val_loader: DataLoader):
-        self.model.eval()
-        loss_iters = 0
-        for x in val_loader:
-            y_hat = self.model(x.x, x.edge_index, x.batch)
-            loss = self.loss_fn(y_hat, x.y)
-            loss_iters += loss.item()
-        self.val_loss.append(loss_iters / len(val_loader))
-        
-        if self.last_val_loss < self.best_val_loss:
-            self.best_val_loss = self.last_val_loss
-            self.save_model()
-            
-        if self.stopper is not None:
-            return self.stopper(self.last_val_loss)
-        return False
-        
-    @property
-    def last_val_loss(self):
-        return self.val_loss[-1] if self.val_loss else 0
-    
-    
-    def save_model(self):
-        out_dir = self.work_dir.joinpath('models')
-        out_dir.mkdir(parents=True, exist_ok=True)
-        params = {
-            'model_state': self.model.state_dict(),
-            'optim_state': self.optimizer.state_dict(),
-            'val_loss': self.last_val_loss,
-        }
-        if self.scheduler is not None:
-            params['scheduler_state'] = self.scheduler.state_dict()
-        model_name = out_dir.joinpath(f'{self.run_name}_best_val_loss_{self.last_val_loss:.4f}.pt')
-        torch.save(params, model_name)
-        
-        self.top_saved_models.append([self.best_val_loss, model_name])
-        self.top_saved_models.sort(key=lambda x: x[0])
-        if len(self.top_saved_models) > self.num_models_save:
-            _, worst_path = self.top_saved_models.pop()
-            if worst_path.is_file():
-                worst_path.unlink()
-            
-
-    def training_summary(self, final_epochs: int):
-        
-        fig_dir = self.work_dir.joinpath('figures')
-        fig_dir.mkdir(parents=True, exist_ok=True)
-        
-        
-        
-        plt.figure()
-        plt.plot(range(final_epochs+1), self.loss_epoch, 'r')
-        plt.plot(range(final_epochs+1), self.val_loss)
-        plt.xlabel('Epoch')
-        plt.ylabel(f'{self.cfg.loss.name}')
-        plt.legend(['Train Loss', 'Validation Loss'])
-        plt.savefig(fig_dir.joinpath('loss_curves.png'))
-        
-        plt.figure()
-        plt.plot(range(self.step), self.lr_history)
-        plt.xlabel('Step')
-        plt.ylabel('Learning Rate')
-        plt.savefig(fig_dir.joinpath('lr.png'))
-        
-        plt.figure()
-        plt.plot(range(self.step), self.step_loss)
-        plt.xlabel('Step')
-        plt.ylabel(f'{self.cfg.loss.name}')
-        plt.savefig(fig_dir.joinpath('step_loss.png'))
         
 
 if __name__ == '__main__':
     
     cfg = get_config()
-    trainer = Train(cfg)
+    trainer = MERFISHTrainer(cfg)
 
     all_files = MERFISHDataset.collect_files(cfg.data.root)
     train_files, val_files, test_files = MERFISHDataset.donor_split(all_files, cfg.data.samplesheet)
--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from .schedulers import get_scheduler
 from .models import get_model
 from .loss_functions import get_loss_fn
 from .dataset import MERFISHDataset
+from .trainer import Trainer
