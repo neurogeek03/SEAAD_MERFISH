b5394d8c9f2a6c1a468a6e62f260a440bc58a19a
brendanwood29 <brendanwwood22@gmail.com>
2026-03-20 18:27:07 +0000

    [feat/wip] improved trainer

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import sys
 import re
 import torch
-from utils import MERFISHDataset, Trainer
+from utils import MERFISHDataset, Trainer, AllMERFISHDataset
 from omegaconf.dictconfig import DictConfig
 from omegaconf.listconfig import ListConfig
 from omegaconf import OmegaConf
@@ -47,9 +47,10 @@ class MERFISHTrainer(Trainer):
         super().__init__(cfg)
         
     def model_forward(self, batch):
-        
-        y_hat = self.model(batch.x, batch.edge_index, batch.batch)
-        loss = self.loss(y_hat, batch.y)
+        # batch = batch.to(self.cfg.device)
+        # print(batch.x.shape)
+        y_hat = self.model(batch.x, batch.edge_index, batch.edge_attr, batch.batch)
+        loss = self.loss_fn(y_hat, batch.y)
         
         return loss, batch.num_graphs 
 
@@ -58,26 +59,36 @@ class MERFISHTrainer(Trainer):
 if __name__ == '__main__':
     
     cfg = get_config()
-    trainer = MERFISHTrainer(cfg)
+    for cell_type in Path('data/braak_xy_expression_by_celltype_donor_lognorm').iterdir():
+        trainer = MERFISHTrainer(cfg)
+        print(cell_type.name)
+        try:
+            all_files = MERFISHDataset.collect_files(cfg.data.root, [cell_type.name])
+            train_files, val_files, test_files = MERFISHDataset.donor_split(all_files, cfg.data.samplesheet)
 
-    all_files = MERFISHDataset.collect_files(cfg.data.root)
-    train_files, val_files, test_files = MERFISHDataset.donor_split(all_files, cfg.data.samplesheet)
+            train_loader = DataLoader(MERFISHDataset(cfg.data.root, train_files, k=cfg.data.k, device=cfg.device),
+                                    batch_size=cfg.batch_size, shuffle=True)
+            val_loader   = DataLoader(MERFISHDataset(cfg.data.root, val_files, k=cfg.data.k, device=cfg.device),
+                                    batch_size=cfg.batch_size, shuffle=False)
+            test_loader  = DataLoader(MERFISHDataset(cfg.data.root, test_files, k=cfg.data.k, device=cfg.device),
+                                    batch_size=cfg.batch_size, shuffle=False)
+            # train_loader = DataLoader(AllMERFISHDataset(cfg.data.root, train_files, k=cfg.data.k),
+            #                         batch_size=cfg.batch_size, shuffle=True)
+            # val_loader   = DataLoader(AllMERFISHDataset(cfg.data.root, val_files, mean=train_loader.dataset.mean, std=train_loader.dataset.std, k=cfg.data.k),
+            #                         batch_size=cfg.batch_size, shuffle=False)
+            # test_loader  = DataLoader(AllMERFISHDataset(cfg.data.root, test_files, k=cfg.data.k),
+            #                         batch_size=cfg.batch_size, shuffle=False)
 
-    train_loader = DataLoader(MERFISHDataset(cfg.data.root, train_files, k=cfg.data.k, device=cfg.device),
-                            batch_size=cfg.batch_size, shuffle=True)
-    val_loader   = DataLoader(MERFISHDataset(cfg.data.root, val_files, k=cfg.data.k, device=cfg.device),
-                            batch_size=cfg.batch_size, shuffle=False)
-    test_loader  = DataLoader(MERFISHDataset(cfg.data.root, test_files, k=cfg.data.k, device=cfg.device),
-                            batch_size=cfg.batch_size, shuffle=False)
-
-    
-    with tqdm(range(cfg.num_epochs)) as pbar:
-        for final_model_epochs in pbar:
-            trainer.train(train_loader)
-            pbar.set_postfix({"train_loss": f"{trainer.loss_epoch[-1]:.4f}", "val_loss": f"{trainer.last_val_loss:.4f}"}, refresh=False)
-            with torch.no_grad():
-                should_stop = trainer.val(val_loader)
-            if should_stop:
-                print(f'Stopped after {final_model_epochs} epochs due to early stopping.')
-                break        
-    trainer.training_summary(final_model_epochs)
+        except Exception:
+            continue
+        
+        with tqdm(range(cfg.num_epochs)) as pbar:
+            for final_model_epochs in pbar:
+                trainer.train(train_loader)
+                pbar.set_postfix({"train_loss": f"{trainer.loss_epoch[-1]:.4f}", "val_loss": f"{trainer.last_val_loss:.4f}"}, refresh=False)
+                with torch.no_grad():
+                    should_stop = trainer.val(val_loader)
+                if should_stop:
+                    print(f'Stopped after {final_model_epochs} epochs due to early stopping.')
+                    break        
+        trainer.training_summary(final_model_epochs)
